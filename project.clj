7b03bfc5c04d6a4e72dd61903921819167b6273d
Alexis Gallagher <alexis@alexisgallagher.com>
2011-10-18 13:05:32 +0100

    Updated project.clj to include all dependencies
    Includes jetty for implementation of servlet-api.
    Includes jetty-runner for running the app
    
    Now you don't need Tomcat or suchlike on your classpath

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject cljvaadin "1.0.0-SNAPSHOT"
   :description "Sample Vaadin web application using Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
-		 [com.vaadin/vaadin "6.6.2"]]
-  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.2"]]
+		 [com.vaadin/vaadin "6.6.2"]
+                 [jetty/jetty "5.1.10"]]
+  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.2"]
+                     [org.mortbay.jetty/jetty-runner "7.0.1.v20091125"]]
   :aot [cljvaadin.VaadinServlet])
