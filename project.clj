65ad290801e9f137aa580aadfb36bf7fb5252408
Alexis Gallagher <alexis@alexisgallagher.com>
2011-10-18 13:21:41 +0100

    updated version of leingen-war

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [com.vaadin/vaadin "6.7.1"]
                  [jetty/jetty "5.1.10"]]
-  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.2"]
+  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.13"]
                      [org.mortbay.jetty/jetty-runner "7.0.1.v20091125"]]
   :aot [cljvaadin.VaadinServlet])
