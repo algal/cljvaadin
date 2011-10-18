f9a110b42ad6ef79fb3535c2cae549c359acbdd8
Alexis Gallagher <alexis@alexisgallagher.com>
2011-10-18 13:12:57 +0100

    updated clojure vaadin version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cljvaadin "1.0.0-SNAPSHOT"
   :description "Sample Vaadin web application using Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-		 [com.vaadin/vaadin "6.6.2"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [com.vaadin/vaadin "6.7.1"]
                  [jetty/jetty "5.1.10"]]
   :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.2"]
                      [org.mortbay.jetty/jetty-runner "7.0.1.v20091125"]]
